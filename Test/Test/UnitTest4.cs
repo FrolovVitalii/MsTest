78df39d204d993d592df31e1729fa0d3f54ba9fa
Vitalii Frolov <vfrolov@deloitte.com>
2019-06-26 01:52:42 -0500

    error emulation

--- Test/Test/UnitTest4.cs
+++ Test/Test/UnitTest4.cs
@@ -44,7 +44,7 @@ namespace Test
         public void TestMethod4_1()
         {
             driver.Url = "http://www.google.com";
-            var picture = driver.FindElements(By.Id("lga"));
+            var picture = driver.FindElements(By.Id("lga---"));
             Debug.WriteLine("Test4_1");
             Console.WriteLine("Test4_1");
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(picture.Count != 0);
